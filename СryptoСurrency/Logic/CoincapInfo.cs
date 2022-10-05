f69e07609edc14bf1e39b66b0b7092f85bf89c70
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-10-05 22:45:35 +0300

    added api call class and coinCapInfo class with getAll, getById, searchByName methods

---
+++ СryptoСurrency/Logic/CoincapInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using СryptoСurrency.Models;
+
+namespace СryptoСurrency.Logic
+{
+    public static class CoincapInfo
+    {
+        const string url = "https://api.coincap.io/v2/";
+
+        public static CoinList GetCoins(int? limit = 10, int? offset = 0)
+        {
+            string urlParameters = $"assets?limit={limit}&offset={offset}";
+
+            var response = APICall.RunAsync<CoinList>(url, urlParameters).GetAwaiter().GetResult();
+
+            return response;
+        }
+
+        public static CoinList GetCoin(string id)
+        {
+            string urlParameters = $"assets?ids={id}";
+
+            var response = APICall.RunAsync<CoinList>(url, urlParameters).GetAwaiter().GetResult();
+
+            return response;
+        }
+
+        public static CoinList SearchCoin(string name)
+        {
+            string urlParameters = $"assets?search={name}";
+
+            var response = APICall.RunAsync<CoinList>(url, urlParameters).GetAwaiter().GetResult();
+
+            return response;
+        }
+    }
+}
