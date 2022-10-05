70ddb7df617b804072aa3b3e758ba51c19cca223
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-10-05 13:30:01 +0300

    add coin model class

---
+++ СryptoСurrency/Models/Coin.cs
@@ -0,0 +1,29 @@
+﻿namespace СryptoСurrency.Models
+{
+    public class Coin
+    {
+        public string Id { get; set; }
+        
+        public int Rank { get; set; }
+
+        public string Symbol { get; set; }
+
+        public string Name { get; set; }
+
+        public double Supply { get; set; }
+
+        public double MaxSupply { get; set; }
+
+        public double MarketCapUsd { get; set; }
+
+        public double VolumeUsd24Hr { get; set; }
+
+        public double PriceUsd { get; set; }
+
+        public double ChangePercent24Hr { get; set; }
+
+        public double Vwap24Hr { get; set; }
+
+        public string Explorer { get; set; }
+    }
+}
